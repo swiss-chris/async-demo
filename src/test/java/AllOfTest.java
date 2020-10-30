b9719d1a3112ed5d7444dcfa6f66784e97367cfb
Chris Dickinson <christopher.dickinson@adthena.com>
2020-10-30 23:07:00 +0100

    firstMatching

--- src/test/java/AllOfTest.java
+++ src/test/java/AllOfTest.java
@@ -11,7 +11,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.toList;
 
 public class AllOfTest {
@@ -38,48 +42,87 @@ public class AllOfTest {
                 }
             ).toArray(CompletableFuture[]::new);
 
-        //// ---- ANY OF ---- ////
-
-        System.out.println("ANY OF (BEGIN)");
-
-        // call all these futures in parallel and store only the first result in another future
-        CompletableFuture
-            .anyOf(pageContentFuturesArray)
-            .thenAccept(o -> {
-                final String string = (String) o;
-                System.out.println(string.length() > 0 ? string : "EMPTY RESPONSE");
-            })
-            .get();
-
-        System.out.println("ANY OF (END)");
-
         //// ---- ALL OF ---- ////
+        CompletableFuture allOf = CompletableFuture.runAsync(() -> {
+            System.out.println("ALL OF (BEGIN)");
 
-        System.out.println("ALL OF (BEGIN)");
+            final String term = "html";
+            CompletableFuture<Long> countFuture = CompletableFuture
+                // call all these futures in parallel ...
+                .allOf(pageContentFuturesArray)
+                // ... and when all responses have been received ...
+                .thenApply(v -> Arrays.stream(pageContentFuturesArray)
+                    // ... join the results into a list of Strings
+                    .map(CompletableFuture::join)
+                    .collect(toList())
+                )
+                // ... and count the number of web pages containing a certain word
+                .thenApply(pageContents -> pageContents
+                    .stream()
+                    .filter(pageContent -> pageContent.toLowerCase().contains(term))
+                    .count()
+                );
+            Long count = 0L;
+            try {
+                count = countFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.format("'%s' was found in %s websites.\r\n", term.toLowerCase(), count);
 
-        // call all these futures in parallel ...
-        final CompletableFuture<List<String>> allPageContentsFuture = CompletableFuture
-            .allOf(pageContentFuturesArray)
-            // ... and when all responses have been received ...
-            .thenApply(v -> Arrays.stream(pageContentFuturesArray)
-                // ... join the results into a list of Strings
-                .map(CompletableFuture::join)
-                .collect(toList())
-            );
+            System.out.println("ALL OF (END)");
+        });
 
-        // Count the number of web pages containing a certain word
-        final String term = "html";
-        CompletableFuture<Long> countFuture = allPageContentsFuture
-            .thenApply(pageContents -> pageContents
-                .stream()
-                .filter(pageContent -> pageContent.toLowerCase().contains(term))
-                .count());
-        System.out.format("'%s' was found in %s websites.\r\n", term.toLowerCase(), countFuture.get());
+        //// ---- ANY OF ---- ////
+        CompletableFuture anyOf = CompletableFuture.runAsync(() -> {
+            System.out.println("ANY OF (BEGIN)");
 
-        System.out.println("ALL OF (END)");
+            // call all these futures in parallel and store only the first result in another future
+            try {
+                CompletableFuture
+                    .anyOf(pageContentFuturesArray)
+                    .thenAccept(o -> {
+                        final String string = (String) o;
+                        System.out.println(string.length() > 0 ? string : "EMPTY RESPONSE");
+                    })
+                    .get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("ANY OF (END)");
+        });
+
+
+        //// ---- FIRST MATCHING ---- ////
+        CompletableFuture firstMatching = CompletableFuture.runAsync(() -> {
+            System.out.println("FIRST MATCHING (BEGIN)");
+
+            // call all these futures in parallel and store only the first result in another future
+            firstMatching(not(String::isEmpty), pageContentFuturesArray)
+                .thenAccept(System.out::println)
+                .orTimeout(2, SECONDS)
+            ;
+
+            System.out.println("FIRST MATCHING (END)");
+        });
+
+        CompletableFuture.allOf(allOf, anyOf, firstMatching).get();
     }
 
     private HttpRequest createRequest(final String url) {
         return HttpRequest.newBuilder(URI.create(url)).header("accept", "text/html").build();
+    }
+
+    private <T> CompletableFuture<T> firstMatching(Predicate<T> predicate, CompletableFuture<T>... futures) {
+        final AtomicBoolean completed = new AtomicBoolean();
+        final CompletableFuture<T> promise = new CompletableFuture<>();
+        for (CompletableFuture<T> future : futures) {
+            future.thenAccept(result -> {
+                if (predicate.test(result) && completed.compareAndSet(false, true))
+                    promise.complete(result);
+            });
+        }
+        return promise;
     }
 }
