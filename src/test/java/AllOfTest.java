a847722c0ae15183f5333cf0967cbb510130bae2
Chris Dickinson <christopher.dickinson@adthena.com>
2020-11-12 18:32:01 +0100

    some improvements

--- src/test/java/AllOfTest.java
+++ src/test/java/AllOfTest.java
@@ -26,15 +26,17 @@ public class AllOfTest {
     public void allOf() throws IOException {
         final var httpClient = HttpClient.newHttpClient();
         final var bodyHandler = HttpResponse.BodyHandlers.ofString();
+        final String searchedString = "html";
 
         // stop watch
         final AtomicLong allOfTime = new AtomicLong();
+        final AtomicLong allOfSimplifiedTime = new AtomicLong();
         final AtomicLong anyOfTime = new AtomicLong();
         final AtomicLong firstMatchTime = new AtomicLong();
 
         // get a list of URLs to call
         final List<String> urls = Files.readAllLines(Paths.get("src/test/resources/urls.txt"));
-        //        final List<String> urls = Files.readAllLines(Paths.get("src/test/resources/urls.withbroken.txt"));
+//                final List<String> urls = Files.readAllLines(Paths.get("src/test/resources/urls.withbroken.txt"));
 
         // map to a list of Futures containing the static html from these urls
         final CompletableFuture<String>[] pageContentFuturesArray = urls
@@ -55,20 +57,20 @@ public class AllOfTest {
             System.out.println("ALL OF (BEGIN)");
             long start = System.currentTimeMillis();
 
-            final String term = "html";
             CompletableFuture<Long> countFuture = CompletableFuture
                 // call all these futures in parallel ...
                 .allOf(pageContentFuturesArray)
                 // ... and when all responses have been received ...
                 .thenApply(v -> Arrays.stream(pageContentFuturesArray)
                     // ... join the results into a list of Strings
+                    // join() is the same as get() but without CHECKED exceptions
                     .map(CompletableFuture::join)
                     .collect(toList())
                 )
                 // ... and count the number of web pages containing a certain word
                 .thenApply(pageContents -> pageContents
                     .stream()
-                    .filter(pageContent -> pageContent.toLowerCase().contains(term))
+                    .filter(contains(searchedString))
                     .count()
                 );
             Long count = 0L;
@@ -80,9 +82,41 @@ public class AllOfTest {
 
             allOfTime.set(System.currentTimeMillis() - start);
 
-            System.out.format("'%s' was found in %s websites.\r\n", term.toLowerCase(), count);
+            System.out.format("'%s' was found in %s websites.\r\n", searchedString.toLowerCase(), count);
 
             System.out.println("ALL OF (END)");
+        });
+
+        //// ---- ALL OF SIMPLIFIED ---- ////
+        CompletableFuture allOfSimplified = CompletableFuture.runAsync(() -> {
+            System.out.println("ALL OF SIMPLIFIED (BEGIN)");
+            long start = System.currentTimeMillis();
+
+            CompletableFuture<Long> countFuture = CompletableFuture
+                // call all these futures in parallel ...
+                .supplyAsync(() -> Arrays.stream(pageContentFuturesArray)
+                    // ... join the results into a list of Strings
+                    // join() is the same as get() but without CHECKED exceptions
+                    .map(CompletableFuture::join)
+                    .collect(toList()))
+                // ... and count the number of web pages containing a certain word
+                .thenApply(pageContents -> pageContents
+                    .stream()
+                    .filter(contains(searchedString))
+                    .count()
+                );
+            Long count = 0L;
+            try {
+                count = countFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            allOfSimplifiedTime.set(System.currentTimeMillis() - start);
+
+            System.out.format("'%s' was found in %s websites.\r\n", searchedString.toLowerCase(), count);
+
+            System.out.println("ALL OF SIMPLIFIED (END)");
         });
 
         //// ---- ANY OF ---- ////
@@ -96,7 +130,7 @@ public class AllOfTest {
                     .anyOf(pageContentFuturesArray)
                     .thenAccept(o -> {
                         final String string = (String) o;
-                        System.out.println(string.length() > 0 ? string : "EMPTY RESPONSE");
+                        System.out.println("ANY OF received a response: " + (string.length() > 0 ? string : "EMPTY RESPONSE"));
                     })
                     .get();
             } catch (InterruptedException | ExecutionException e) {
@@ -114,8 +148,10 @@ public class AllOfTest {
             long start = System.currentTimeMillis();
 
             // call all these futures in parallel and store only the first result in another future
-            firstMatching(not(String::isEmpty), pageContentFuturesArray)
-                .thenAccept(System.out::println)
+            firstMatching(not(String::isEmpty).and(contains(searchedString)), pageContentFuturesArray)
+                .thenAccept(nonEmptyResponse -> {
+                    System.out.format("FIRST MATCHING received non empty response containing '%s': %s\r\n", searchedString, nonEmptyResponse);
+                })
                 .orTimeout(2, SECONDS)
             ;
 
@@ -126,22 +162,28 @@ public class AllOfTest {
         try {
             // don't use .orTimeout() !!!
             // https://stackoverflow.com/questions/64807597/unexpected-behavior-for-completablefuture-allof-ortimeout
-            CompletableFuture.allOf(allOf, anyOf, firstMatching).get();
-//            CompletableFuture.allOf(allOf, anyOf, firstMatching).get(5, SECONDS);
+//            CompletableFuture.allOf(allOf, allOfSimplified, anyOf, firstMatching).get();
+            CompletableFuture.allOf(allOf, allOfSimplified, anyOf, firstMatching).get(5, SECONDS);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             System.out.format("ALL OF execution time: %s milliseconds.\r\n", allOfTime);
+            System.out.format("ALL OF SIMPLIFIED execution time: %s milliseconds.\r\n", allOfSimplifiedTime);
             System.out.format("ANY OF OF execution time: %s milliseconds.\r\n", anyOfTime);
             System.out.format("FIRST MATCH execution time: %s milliseconds.\r\n", firstMatchTime);
         }
 
     }
 
+    private Predicate<String> contains(String partialString) {
+        return entireString -> entireString.toLowerCase().contains(partialString);
+    }
+
     private HttpRequest createRequest(final String url) {
         return HttpRequest.newBuilder(URI.create(url)).header("accept", "text/html").build();
     }
 
+    // copied from https://www.nurkiewicz.com/2013/12/promises-and-completablefuture.html
     private <T> CompletableFuture<T> firstMatching(Predicate<T> predicate, CompletableFuture<T>... futures) {
         final AtomicBoolean completed = new AtomicBoolean();
         final CompletableFuture<T> promise = new CompletableFuture<>();
